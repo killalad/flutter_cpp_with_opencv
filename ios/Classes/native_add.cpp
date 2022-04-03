0aebf71efacc731d86f256564263a5314ef32b81
Martin Georgiu <martin.georgiu@gmail.com>
2022-04-03 11:46:10 +0200

    aruco test

--- ios/Classes/native_add.cpp
+++ ios/Classes/native_add.cpp
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include <opencv2/core.hpp>
+#include <opencv2/aruco.hpp>
 
 extern "C" __attribute__((visibility("default"))) __attribute__((used))
 int32_t native_add(int32_t x, int32_t y) {
     cv::Mat m = cv::Mat::zeros(x, y, CV_8UC3);
     return m.rows + m.cols;
+    auto tt = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
 }
